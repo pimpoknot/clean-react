dbbe618ada0f4bc0eb1e6250f1c610543ab86df5
Marcos Maverick <marcos@internit.com.br>
2021-02-10 22:28:53 -0300

    feat: ensure RemoteAuthentication throws UnexpectedError on default cases

--- src/domain/data/usecases/authentication/remote-authentication.spec.ts
+++ src/domain/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,6 +4,7 @@ import { mockAuthentication } from "@/domain/test/mock-authentication"
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credencitals-error"
 import { HttpStatusCode } from "../../protocols/http/http-response"
 import faker from 'faker'
+import { UnexpectedError } from "@/domain/errors/unexpected-error"
 
 type SutTypes ={
     sut: RemoteAuthentication
@@ -44,4 +45,32 @@ describe('RemoteAuthentication', ()=>{
         const promise = sut.auth(mockAuthentication())
         await expect(promise).rejects.toThrow(new InvalidCredentialsError())
     })
+
+    test('Should throw UnexpectedError if HttpPostClient Returns 400', async()=>{
+        const {sut, httpPostClientSpy} = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.badRequest
+        }
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+
+    test('Should throw UnexpectedError if HttpPostClient Returns 500', async()=>{
+        const {sut, httpPostClientSpy} = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.serverError
+        }
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+
+    test('Should throw UnexpectedError if HttpPostClient Returns 404', async()=>{
+        const {sut, httpPostClientSpy} = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.notFound
+        }
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+    
 })
--- src/domain/data/usecases/authentication/remote-authentication.ts
+++ src/domain/data/usecases/authentication/remote-authentication.ts
@@ -1,5 +1,6 @@
 import { HttpPostClient } from '@/domain/data/protocols/http/http-post-client';
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credencitals-error';
+import { UnexpectedError } from '@/domain/errors/unexpected-error';
 import { AuthenticationParams } from '@/domain/usecases/authentication';
 import { deflate } from 'zlib';
 import { HttpStatusCode } from '../../protocols/http/http-response';
@@ -17,8 +18,9 @@ export class RemoteAuthentication{
             body: params
         })
         switch(httpResponse.statusCode){
+            case HttpStatusCode.ok: break
             case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
-            default: return Promise.resolve()
+            default: throw new UnexpectedError()
         }
         
     }
